e430077faf589151505abdb9d4edaab1307a86bb
Dario <dariosans@gmail.com>
2023-10-26 01:15:20 -0400

    echo is done with no test

---
+++ echo/src/main.rs
@@ -0,0 +1,28 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "echo")]
+#[command(author = "Dario S. github.com/dotshix")]
+#[command(version = "0.1.0")]
+#[command(about = "Rust echo implementation", long_about = None)]
+struct CLI {
+    #[arg(short)]
+    /// Omits newline
+    n: bool,
+
+    #[arg(short)]
+    bob: Option<String>,
+
+    #[arg(required = true)]
+    /// Input text
+    text: Vec<String>,
+}
+
+fn main() {
+    let cli = CLI::parse();
+    let ending = if cli.n { "" }  else { "\n" };
+
+    let output = if cli.text.len() == 1 { cli.text[0].clone() } else { cli.text.join(" ") };
+
+    print!("{}{}", output, ending);
+}
