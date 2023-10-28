794862e41910b211325d84c31953f941dc14164f
Dario <dariosans@gmail.com>
2023-10-27 21:19:35 -0400

    Fixed issue where -- wasnt working

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -10,16 +10,35 @@ struct CLI {
     /// Omits newline
     n: bool,
 
-    #[arg(required = true)]
+    #[arg(required = false, allow_hyphen_values = true)]
     /// Input text
     text: Vec<String>,
 }
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let mut found_double_dash = false;
+
+    // Check if the arguments contain "--" and it is not the first argument (which is the program name)
+    if (args.len() == 2 && args[1] == "--") || (args.len() == 3 && args[1] == "-n" && args[2] == "--") {
+        found_double_dash = true;
+    }
+
+    // Parse the arguments with clap
     let cli = CLI::parse();
-    let ending = if cli.n { "" }  else { "\n" };
 
-    let output = if cli.text.len() == 1 { cli.text[0].clone() } else { cli.text.join(" ") };
+    // Determine the line ending and output text
+    let ending = if cli.n { "" } else { "\n" };
+    let output = if cli.text.len() == 1 {
+        cli.text[0].clone()
+    } else {
+        cli.text.join(" ")
+    };
 
-    print!("{}{}", output, ending);
+    // Print the output
+    if found_double_dash {
+        print!("--{}", ending);
+    } else {
+        print!("{}{}", output, ending);
+    }
 }
